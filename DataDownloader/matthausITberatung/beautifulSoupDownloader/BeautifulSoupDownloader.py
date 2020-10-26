e0bca3034cc22f784da0f37b263d0a2d547b89b2
Maciej Pietruszewski <mac.pietruszewski@gmail.com>
2020-10-26 21:13:46 +0100

    Add BeautifulSoupDownloader.py module

--- DataDownloader/matthausITberatung/beautifulSoupDownloader/BeautifulSoupDownloader.py
+++ DataDownloader/matthausITberatung/beautifulSoupDownloader/BeautifulSoupDownloader.py
@@ -2,19 +2,17 @@ import requests
 
 from bs4 import BeautifulSoup
 
-from matthausITberatung.URLs.URLprovider import URLprovider
+class BeautifulSoupDownloader:
 
-text = 'downloaded3'
-urlProvider = URLprovider()
-listOfWizzAirURLs = urlProvider.getListOfWizzAirURLs(urlProvider.LUFTHANSA())
+    def getURLcontent(self, url):
+        return BeautifulSoup(requests.get(url).content, "html.parser")
 
-listOfHTMLtags = ['div']
-print(listOfHTMLtags)
-with open(str(text) + '.txt', 'w', encoding='utf-8') as outfile:
-    for url in listOfWizzAirURLs:
-        soup = BeautifulSoup(requests.get(url).content, "html.parser")
-        text = [' '.join(s.findAll(text=True)) for s in soup.find_all(listOfHTMLtags)]
-        for item in text:
-            print(item, file=outfile)
+    def extractData(self, url, listOfHTMLtags):
+        return [' '.join(s.findAll(text=True)) for s in self.getURLcontent(url).find_all(listOfHTMLtags)]
 
-        print("Done! File is saved where you have your scrape-website.py")
+    def putExtractedDataIntoFile(self, fileName, listOfHTMLtags, listOfAirLineURLs):
+        with open(str(fileName) + '.txt', 'w', encoding='utf-8') as outfile:
+            for url in listOfAirLineURLs:
+                text = self.extractData(url, listOfHTMLtags)
+                for item in text:
+                    print(item, file=outfile)
