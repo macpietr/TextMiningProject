cabfc112e5161afc659dc0e44efb65865ce4978d
Maciej Pietruszewski <mac.pietruszewski@gmail.com>
2020-11-03 22:13:26 +0100

    Change order of methods, due to clean the code

--- DataDownloader/matthausITberatung/URLs/URLprovider.py
+++ DataDownloader/matthausITberatung/URLs/URLprovider.py
@@ -9,11 +9,11 @@ class URLprovider:
     def LUFTHANSA(self):
         return 'lufthansa'
 
-    def getURL(iterator, airLineName):
-        return "https://www.airlinequality.com/airline-reviews/"+airLineName+"/page/"+str(iterator)+"/?sortby=post_date%3ADesc&pagesize=100"
-
     def getListOfAirLineURLs(self, airlineName):
         listOfAirLineURLs = []
         for iterator in range(1, 11):
             listOfAirLineURLs.append(URLprovider.getURL(iterator, airlineName))
         return listOfAirLineURLs
+
+    def getURL(iterator, airLineName):
+        return "https://www.airlinequality.com/airline-reviews/"+airLineName+"/page/"+str(iterator)+"/?sortby=post_date%3ADesc&pagesize=100"
--- DataDownloader/matthausITberatung/beautifulSoupDownloader/BeautifulSoupDownloader.py
+++ DataDownloader/matthausITberatung/beautifulSoupDownloader/BeautifulSoupDownloader.py
@@ -4,15 +4,15 @@ from bs4 import BeautifulSoup
 
 class BeautifulSoupDownloader:
 
-    def getURLcontent(self, url):
-        return BeautifulSoup(requests.get(url).content, "html.parser")
-
-    def extractData(self, url, listOfHTMLtags):
-        return [' '.join(s.findAll(text=True)) for s in self.getURLcontent(url).find_all(listOfHTMLtags)]
-
     def putExtractedDataIntoFile(self, fileName, listOfHTMLtags, listOfAirLineURLs):
         with open(str(fileName) + '.txt', 'w', encoding='utf-8') as outfile:
             for url in listOfAirLineURLs:
                 text = self.extractData(url, listOfHTMLtags)
                 for item in text:
                     print(item, file=outfile)
+
+    def extractData(self, url, listOfHTMLtags):
+        return [' '.join(s.findAll(text=True)) for s in self.getURLcontent(url).find_all(listOfHTMLtags)]
+
+    def getURLcontent(self, url):
+        return BeautifulSoup(requests.get(url).content, "html.parser")
