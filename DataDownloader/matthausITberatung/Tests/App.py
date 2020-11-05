611b6ade1866f4db5a87b4e5674b50e3cbde5165
Maciej Pietruszewski <mac.pietruszewski@gmail.com>
2020-11-05 19:02:43 +0100

    Test extraction of main opinions from webPage

--- DataDownloader/matthausITberatung/Tests/App.py
+++ DataDownloader/matthausITberatung/Tests/App.py
@@ -1,9 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
-from matthausITberatung.Tests.DataGetter import DataGetter
-
-
-url = 'https://www.airlinequality.com/airline-reviews/lufthansa/page/1/?sortby=post_date%3ADesc&pagesize=100%22'
+url = 'https://www.airlinequality.com/airline-reviews/lufthansa/page/1/?sortby=post_date%3ADesc&pagesize=100'
 beautifulSoupObject = BeautifulSoup(requests.get(url).content, "html.parser")
-print(beautifulSoupObject.find('div', id="anchor726666").find('h2').find_all(text=True))
+tag = beautifulSoupObject.find('div', id=re.compile("anchor")).find('div').find('div').find_all(text=True)[2]
+tag2 = tag.replace("|  ","")
+print(tag2)
