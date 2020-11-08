4d41478fe9b6190d0011142bdf247edc44a486c4
Maciej Pietruszewski <mac.pietruszewski@gmail.com>
2020-11-08 12:02:15 +0100

    Download only main opinions from one page

--- DataDownloader/matthausITberatung/Tests/App.py
+++ DataDownloader/matthausITberatung/Tests/App.py
@@ -4,6 +4,16 @@ import re
 
 url = 'https://www.airlinequality.com/airline-reviews/lufthansa/page/1/?sortby=post_date%3ADesc&pagesize=100'
 beautifulSoupObject = BeautifulSoup(requests.get(url).content, "html.parser")
-tag = beautifulSoupObject.find('div', id=re.compile("anchor")).find('div').find('div').find_all(text=True)[2]
-tag2 = tag.replace("|  ","")
-print(tag2)
+tag = beautifulSoupObject.findAll('div', id=re.compile("anchor"))
+for t in tag:
+    userPost = t.find('div').find('div').find_all(text=True)
+    userPostLength = len(userPost)
+    print(userPost[userPostLength-1].replace("|","").lstrip().rstrip())
+    # if userPost[0]=='Not Verified':
+    #     print(userPost[1])
+    # else:
+    #     print(userPost[2])
+
+    # .find('div').find('div')
+        # .find_all(text=True)[2]
+# tag2 = tag.replace("| ","")
