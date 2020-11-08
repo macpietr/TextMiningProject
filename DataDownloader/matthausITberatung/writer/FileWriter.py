6dcb80b7cea86887150dafb9bb40279ae618f49e
Maciej Pietruszewski <mac.pietruszewski@gmail.com>
2020-11-08 22:57:18 +0100

    Insert into file data which is provided as array of arrays

--- DataDownloader/matthausITberatung/writer/FileWriter.py
+++ DataDownloader/matthausITberatung/writer/FileWriter.py
@@ -1,6 +1,7 @@
 class FileWriter:
 
-    def putExtractedDataIntoFile(self, fileName, extractedData):
+    def putExtractedDataIntoFile(self, fileName, arrayOfArraysData):
         with open(str(fileName) + '.txt', 'w', encoding='utf-8') as outfile:
-            for item in extractedData:
-                print(item, file=outfile)
+            for dataArray in arrayOfArraysData:
+                for dataItem in dataArray:
+                    print(dataItem, file=outfile)
