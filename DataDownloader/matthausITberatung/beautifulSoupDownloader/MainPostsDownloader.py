48cdd4e0a685c06b7b0e22f55139d20b8b9258c4
Maciej Pietruszewski <mac.pietruszewski@gmail.com>
2020-11-08 12:23:49 +0100

    Add MainPostsDownloader

---
+++ DataDownloader/matthausITberatung/beautifulSoupDownloader/MainPostsDownloader.py
@@ -0,0 +1,13 @@
+import re
+
+
+class MainPostsDownloader:
+
+    def downloadAllPostsFromPage(self):
+
+    def downloadPost(self):
+
+    def downloadAllTaggedData(self, beautifulSoupObject):
+        return beautifulSoupObject.findAll('div', id=re.compile("anchor"))
+
+
