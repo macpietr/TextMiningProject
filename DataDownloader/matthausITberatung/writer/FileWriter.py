5b2dcd0c9a0de21e15b913de504c95bdb8bff013
Maciej Pietruszewski <mac.pietruszewski@gmail.com>
2020-11-09 21:09:00 +0100

    Change output files destination

--- DataDownloader/matthausITberatung/writer/FileWriter.py
+++ DataDownloader/matthausITberatung/writer/FileWriter.py
@@ -1,7 +1,9 @@
 class FileWriter:
 
+    def PROJECT_DIRECTORY(self):
+        return 'C:\\Users\\macpi\\Dokumenty\\magisterka\\PythonProjects\\DataDownloader\\output_files\\'
     def putExtractedDataIntoFile(self, fileName, arrayOfArraysData):
-        with open(str(fileName) + '.txt', 'w', encoding='utf-8') as outfile:
+        with open(str(self.PROJECT_DIRECTORY() + fileName) + '.txt', 'w', encoding='utf-8') as outfile:
             for dataArray in arrayOfArraysData:
                 for dataItem in dataArray:
                     print(dataItem, file=outfile)
