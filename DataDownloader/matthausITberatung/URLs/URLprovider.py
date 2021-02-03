bb2fb692e563b6973fea89d4b38e4f72ea09c209
Maciej Pietruszewski <mac.pietruszewski@gmail.com>
2021-02-03 21:26:22 +0100

    add TODO

--- DataDownloader/matthausITberatung/URLs/URLprovider.py
+++ DataDownloader/matthausITberatung/URLs/URLprovider.py
@@ -11,6 +11,7 @@ class URLprovider:
 
     def getListOfAirLineURLs(self, airlineName):
         listOfAirLineURLs = []
+        # TODO change range for iterator
         for iterator in range(1, 11):
             listOfAirLineURLs.append(URLprovider.getURL(iterator, airlineName))
         return listOfAirLineURLs
