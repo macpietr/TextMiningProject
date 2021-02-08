b73567e240f4b9dec8d5c918eb3e8f2ac0b26ef5
Maciej Pietruszewski <mac.pietruszewski@gmail.com>
2021-02-08 19:42:32 +0100

    Fix the bug. Found the parsing solution for ReviewRating type table

--- DataDownloader/matthausITberatung/dataDownloader/ReviewRatingDownloader.py
+++ DataDownloader/matthausITberatung/dataDownloader/ReviewRatingDownloader.py
@@ -2,19 +2,21 @@ from matthausITberatung.cleaner.DataCleaner import DataCleaner
 from matthausITberatung.dataDownloader.AbstractDownloader import AbstractDownloader
 
 
-class AircraftDownloader(AbstractDownloader):
+class ReviewRatingDownloader(AbstractDownloader):
 
-    def __init__(self, listOfBeautifulSoupObjects):
+    def __init__(self, listOfBeautifulSoupObjects, reviewRatingHeader):
         super().__init__(listOfBeautifulSoupObjects)
+        self.reviewRatingHeader = reviewRatingHeader
 
     def getDataBasedOnHTMLtype(self, beautifulSoupObject):
-        return beautifulSoupObject.findAll('table',class_='review-ratings')
+        return beautifulSoupObject.findAll('tr')
 
     def getExtractedRow(self, item):
-        trtag = item.find('tr')
-        if('Aircraft' in str(trtag)):
-            result = trtag.find('td',class_='review-value').findAll(text=True)
-            return result
+        td = item.findAll('td')
+        if (self.reviewRatingHeader in str(td)):
+            return td[1].findAll(text=True)
+            # TODO naprawić to (ma być zgodnie z testem App.py)
+
 
     def getProcessedRow(self, userMainMark):
         userMainMarkAsString = str(userMainMark)
