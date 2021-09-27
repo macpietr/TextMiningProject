f6ec786933eca7db6bc8d1df4d7976fcf5af12db
macpietr <mac.pietruszewski@gmail.com>
2021-09-27 18:10:40 +0200

    clean two cases of dataset for every airline and save the results

--- matthausITberatung/dataCleaner/app/Main.py
+++ matthausITberatung/dataCleaner/app/Main.py
@@ -1,14 +1,22 @@
-from matthausITberatung.dataCleaner.cleaner.GeneralDataCleaner import GeneralDataCleaner
+from matthausITberatung.dataCleaner.cleaner.DataCleaner import DataCleaner
 from matthausITberatung.fileManager.FileReader import FileReader
 from matthausITberatung.fileManager.FileWriter import FileWriter
 from matthausITberatung.fileManager.PathsManager import PathsManager
 
-saveFolder = PathsManager().CLEANED_DATA_FILES_DIR()
-downloadedFolder = PathsManager().DOWNLOADED_FILES_DIR()
-listOfCasesRequiredCleaning = ['MainUserOpinion','MainMarkInOpinion']
 
-for airlineName in PathsManager().LIST_OF_AIRLINES():
-    for case in listOfCasesRequiredCleaning:
-        readFile = FileReader().readFile(downloadedFolder, case, airlineName)
-        cleanedData = GeneralDataCleaner(readFile).cleanData()
-        FileWriter(airlineName, case, saveFolder).putDataIntoFile(cleanedData)
+downloadedFolder = PathsManager().DOWNLOADED_FILES_DIR()
+saveFolder = PathsManager().CLEANED_DATA_FILES_DIR()
+mainUserOpinionChildDir = 'MainUserOpinion'
+mainMarkInOpinionDir = 'MainMarkInOpinion'
+
+#Clean data for MainUserOpinion
+for airline in PathsManager().LIST_OF_AIRLINES():
+    downloadedData = FileReader().readFile(downloadedFolder, mainUserOpinionChildDir, airline)
+    cleanedData = DataCleaner().cleanDataExtended(downloadedData)
+    FileWriter(airline, mainUserOpinionChildDir, saveFolder).putDataIntoFile(cleanedData)
+
+#Clean data for MainMarkInOpinion
+for airline in PathsManager().LIST_OF_AIRLINES():
+    downloadedData = FileReader().readFile(downloadedFolder, mainMarkInOpinionDir, airline)
+    cleanedData = DataCleaner().cleanData(downloadedData)
+    FileWriter(airline, mainMarkInOpinionDir, saveFolder).putDataIntoFile(cleanedData)
