a6cfc5fc9c63bd9ca69437725272f8981e24d07e
macpietr <mac.pietruszewski@gmail.com>
2021-09-27 22:52:24 +0200

    add regex to cleanDataExtended

--- matthausITberatung/dataCleaner/cleaner/DataCleaner.py
+++ matthausITberatung/dataCleaner/cleaner/DataCleaner.py
@@ -6,8 +6,10 @@ class DataCleaner:
 
     def cleanDataExtended(self, data):
         data = data.lower()
-        data = re.sub('[%s]' % re.escape(string.punctuation), '', data)
-        data = re.sub('\w*\d\w*', '', data)
+        data = re.sub('[%s]' % re.escape(string.punctuation), '', data) #remove interpunction
+        data = re.sub('[''""...]', '', data)
+        data = re.sub('\w*\d\w*', '', data) #remove numbers
+        data = re.sub('\n', '', data)
         return data
 
     def cleanData(self, data):
