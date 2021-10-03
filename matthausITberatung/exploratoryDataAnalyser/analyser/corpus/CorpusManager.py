bf0e47158d9767995742144292ec3934a22e4311
macpietr <mac.pietruszewski@gmail.com>
2021-10-03 12:37:26 +0200

    refactor corpus class

--- matthausITberatung/exploratoryDataAnalyser/analyser/corpus/CorpusManager.py
+++ matthausITberatung/exploratoryDataAnalyser/analyser/corpus/CorpusManager.py
@@ -1,20 +1,19 @@
 import pandas
-import pickle
 from pandas import DataFrame
 
-from matthausITberatung.exploratoryDataAnalyser.analyser.corpus.CleanedDataDict import CleanedDataDict
+from matthausITberatung.exploratoryDataAnalyser.analyser.corpus.CleanedDataDictManager import CleanedDataDictManager
 from matthausITberatung.fileManager.PathsManager import PathsManager
 
 
 class CorpusManager:
 
-    def createCorpus(self, *numberOfCharactersToPrint):
-        cleanDataDictForCorpus = CleanedDataDict().getCleanedDataDictForCorpus()
+    def createMainOpinionsCorpus(self, *numberOfCharactersToPrint):
+        cleanDataDictForMainOpinionsCorpus = CleanedDataDictManager().getCleanedDataDictForCorpus(partOfScrappedData='MainUserOpinion')
         if len(numberOfCharactersToPrint) == 0:
-            return DataFrame.from_dict(cleanDataDictForCorpus).transpose()
+            return DataFrame.from_dict(cleanDataDictForMainOpinionsCorpus).transpose()
         if len(numberOfCharactersToPrint) == 1:
             pandas.set_option('max_colwidth',numberOfCharactersToPrint[0])
-            return DataFrame.from_dict(cleanDataDictForCorpus).transpose()
+            return DataFrame.from_dict(cleanDataDictForMainOpinionsCorpus).transpose()
         elif len(numberOfCharactersToPrint) > 1:
             print('Maximum number of parameters is 1 for getCorpus() method')
 
