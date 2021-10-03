3dcdb4067e933bf1983e8da7a646268345c71d14
macpietr <mac.pietruszewski@gmail.com>
2021-10-03 12:36:57 +0200

    refactor CleanedDataDict

--- matthausITberatung/exploratoryDataAnalyser/analyser/corpus/CleanedDataDictManager.py
+++ matthausITberatung/exploratoryDataAnalyser/analyser/corpus/CleanedDataDictManager.py
@@ -2,16 +2,16 @@ from matthausITberatung.dataCleaner.cleaner.DataCleaner import DataCleaner
 from matthausITberatung.fileManager.FileReader import FileReader
 from matthausITberatung.fileManager.PathsManager import PathsManager
 
-class CleanedDataDict:
 
+class CleanedDataDictManager:
 
-    def getCleanedDataDictForCorpus(self):
-        cleanedDataDict = self.getCleanedDataDict()
+    def getCleanedDataDictForCorpus(self, partOfScrappedData):
+        cleanedDataDict = self.getCleanedDataDict(partOfScrappedData)
         return {key: [cleanedDataDict[key]] for (key, value) in cleanedDataDict.items()}
 
-    def getCleanedDataDict(self):
+    def getCleanedDataDict(self, partOfScrappedData):
         cleanedDataDict = {}
         for i, airline in enumerate(PathsManager().LIST_OF_AIRLINES()):
-            downloadedData = FileReader().readFile(PathsManager().CLEANED_DATA_FILES_DIR(), 'MainUserOpinion', airline)
+            downloadedData = FileReader().readFile(PathsManager().CLEANED_DATA_FILES_DIR(), partOfScrappedData, airline)
             cleanedDataDict[airline] = DataCleaner().cleanData(downloadedData)
         return cleanedDataDict
