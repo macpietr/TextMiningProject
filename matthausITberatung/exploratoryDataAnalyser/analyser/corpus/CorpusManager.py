5f5da4243a19367ddef4a7f90a6b13ac4468ff76
macpietr <mac.pietruszewski@gmail.com>
2021-10-03 09:21:01 +0200

    Change class from corpus to corpusManager

--- matthausITberatung/exploratoryDataAnalyser/analyser/corpus/CorpusManager.py
+++ matthausITberatung/exploratoryDataAnalyser/analyser/corpus/CorpusManager.py
@@ -1,11 +1,14 @@
 import pandas
+import pickle
 from pandas import DataFrame
 
 from matthausITberatung.exploratoryDataAnalyser.analyser.corpus.CleanedDataDict import CleanedDataDict
+from matthausITberatung.fileManager.PathsManager import PathsManager
 
-class Corpus:
 
-    def getCorpus(self, *numberOfCharactersToPrint):
+class CorpusManager:
+
+    def createCorpus(self, *numberOfCharactersToPrint):
         cleanDataDictForCorpus = CleanedDataDict().getCleanedDataDictForCorpus()
         if len(numberOfCharactersToPrint) == 0:
             return DataFrame.from_dict(cleanDataDictForCorpus).transpose()
@@ -14,3 +17,9 @@ class Corpus:
             return DataFrame.from_dict(cleanDataDictForCorpus).transpose()
         elif len(numberOfCharactersToPrint) > 1:
             print('Maximum number of parameters is 1 for getCorpus() method')
+
+    def saveCorpus(self, corpusObject, objectfileName):
+        corpusObject.to_pickle(PathsManager().PICKLED_FILES()+'/'+objectfileName+'.pkl')
+
+    def getSavedCorpus(self, objectFileName):
+        return pandas.read_pickle(PathsManager().PICKLED_FILES()+'/'+objectFileName+'.pkl')
