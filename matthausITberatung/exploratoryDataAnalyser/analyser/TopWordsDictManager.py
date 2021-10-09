c3d12783d21868c15b4b7350017840bee0b3a421
macpietr <mac.pietruszewski@gmail.com>
2021-10-09 14:57:53 +0200

    creatre TopWordsDictManager

---
+++ matthausITberatung/exploratoryDataAnalyser/analyser/TopWordsDictManager.py
@@ -0,0 +1,14 @@
+class TopWordsDictManager:
+
+    def getTopWordsDict(self, DataTermMatrix):
+        topWorldsDict = {}
+        for column in DataTermMatrix.columns:
+            top = DataTermMatrix[column].sort_values(ascending=False)
+            topWorldsDict[column] = list(zip(top.index, top.values))
+        return topWorldsDict
+
+    def printTopWords(self, topWordsDict, numberOfWordsToPrint):
+        for key in topWordsDict:
+            print(key)
+            print('; '.join([word + ':' + str(count) for word, count in topWordsDict[key][0:numberOfWordsToPrint]]))
+            print('-----')
