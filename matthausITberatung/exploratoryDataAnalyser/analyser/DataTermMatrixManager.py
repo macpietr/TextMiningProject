b668df52571834823bd9bb518ef7896f99962263
macpietr <mac.pietruszewski@gmail.com>
2021-10-14 22:41:15 +0200

    encapsulate methods in DataTermMatrixManager

--- matthausITberatung/exploratoryDataAnalyser/analyser/DataTermMatrixManager.py
+++ matthausITberatung/exploratoryDataAnalyser/analyser/DataTermMatrixManager.py
@@ -8,14 +8,14 @@ class DataTermMatrixManager:
         self.countVectoriser = CountVectorizer(stop_words='english')
 
     def createDataTermMatrix(self, corpusColumn):
-        return pandas.DataFrame(self.getCountedWordsArray(corpusColumn), columns=self.getCountedWordsLabels())
+        return pandas.DataFrame(self.__getCountedWordsArray(corpusColumn), columns=self.__getCountedWordsLabels())
 
-    def getCountedWordsLabels(self):
+    def __getCountedWordsLabels(self):
         return self.countVectoriser.get_feature_names_out()
 
-    def getCountedWordsArray(self, corpusColumn):
-        return self.getCountedWordsVector(corpusColumn).toarray()
+    def __getCountedWordsArray(self, corpusColumn):
+        return self.__getCountedWordsVector(corpusColumn).toarray()
 
-    def getCountedWordsVector(self, corpusColumn):
+    def __getCountedWordsVector(self, corpusColumn):
         return self.countVectoriser.fit_transform(corpusColumn)
 
