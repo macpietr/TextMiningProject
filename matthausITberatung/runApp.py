5ae7e4be90bd0a0ab96a629e878aeb99151aeed5
macpietr <mac.pietruszewski@gmail.com>
2021-11-16 20:14:18 +0100

    create script wchich will be running whole main scripts from modules

---
+++ matthausITberatung/runApp.py
@@ -0,0 +1,7 @@
+print('run all modules')
+print('###############')
+print('run data downloader module')
+from matthausITberatung.dataDownloader.app import Main
+print('Run dataCleaner module')
+from matthausITberatung.dataCleaner.app import Main
+print('All modules were executed')
