705902def36d48bebd7784f2b25182b90dcdb10e
macpietr <mac.pietruszewski@gmail.com>
2021-11-24 20:20:39 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='TextMiningProject',
+    version='',
+    packages=['matthausITberatung', 'matthausITberatung.stopWords', 'matthausITberatung.stopWords.app',
+              'matthausITberatung.stopWords.manager', 'matthausITberatung.dataCleaner',
+              'matthausITberatung.dataCleaner.app', 'matthausITberatung.dataCleaner.cleaner',
+              'matthausITberatung.plotsManager', 'matthausITberatung.plotsManager.app',
+              'matthausITberatung.dataDownloader', 'matthausITberatung.dataDownloader.app',
+              'matthausITberatung.dataDownloader.URLs', 'matthausITberatung.dataDownloader.Tests',
+              'matthausITberatung.dataDownloader.dataDownloader', 'matthausITberatung.objectsManager',
+              'matthausITberatung.exploratoryDataAnalyser', 'matthausITberatung.exploratoryDataAnalyser.app',
+              'matthausITberatung.exploratoryDataAnalyser.analyser'],
+    url='',
+    license='',
+    author='Maciej Pietruszewski',
+    author_email='',
+    description=''
+)
