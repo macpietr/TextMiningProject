1de65e2d83c4204cdcd78d3400c055064084dd6e
macpietr <mac.pietruszewski@gmail.com>
2021-11-30 21:59:23 +0100

    add collocations=False

--- matthausITberatung/plotsManager/app/Wordcloud.py
+++ matthausITberatung/plotsManager/app/Wordcloud.py
@@ -2,19 +2,19 @@ from wordcloud import WordCloud
 from matplotlib import pyplot as plt
 
 from matthausITberatung.objectsManager.ObjectsManager import ObjectsManager
+from matthausITberatung.objectsManager.PathsManager import PathsManager
 
 objectsManager = ObjectsManager()
 
-topWordsDict = objectsManager.getSavedObject('topWordsDict')
 UNION_STOP_WORDS = objectsManager.getSavedObject('unionStopWords')
-mainOpinionsCorpus = objectsManager.getSavedObject('mainOpinionsCorpus')
+mainOpinionsCorpus = objectsManager.getSavedObject('bigramsCorpus')
 
 
-projectWordcloud = WordCloud(stopwords=UNION_STOP_WORDS, background_color="white", colormap="Dark2", max_font_size=150, random_state=42)
+projectWordcloud = WordCloud(stopwords=UNION_STOP_WORDS, collocations=False, background_color="white", colormap="Dark2", max_font_size=150, random_state=42)
 
 plt.rcParams['figure.figsize'] = [50,50]
 
-for numberOfPlot, airline in enumerate(topWordsDict.keys()):
+for numberOfPlot, airline in enumerate(PathsManager().LIST_OF_AIRLINES):
     projectWordcloud.generate(mainOpinionsCorpus.opinions[airline])
     plt.subplot(3,4,numberOfPlot+1)
     plt.imshow(projectWordcloud, interpolation="bilinear")
