94250fbd7e39ed263b4ffb490f7b4cf8912b933f
macpietr <mac.pietruszewski@gmail.com>
2021-12-08 11:22:07 +0100

    refactor class name

--- matthausITberatung/exploratoryDataAnalyser/analyser/DataDictManager.py
+++ matthausITberatung/exploratoryDataAnalyser/analyser/DataDictManager.py
@@ -7,8 +7,18 @@ class CleanedDataDictManager:
     def getDataDictForCorpus(self, dataDict):
         return {key: [dataDict[key]] for (key, value) in dataDict.items()}
 
-    def getCleanedDataDictFromFiles(self, partOfScrappedData):
+    def getDataDictFromFiles(self, partOfScrappedData):
         cleanedDataDict = {}
         for i, airline in enumerate(PathsManager().LIST_OF_AIRLINES):
             cleanedDataDict[airline] = FileReader().readFile(PathsManager().CLEANED_DATA_FILES_DIR, partOfScrappedData, airline)
         return cleanedDataDict
+
+    def getcleandDataDictForCorpusWithoutStopWords(self, dataDict, stopWords):
+        cleandDataDictForCorpusWithoutStopWords = {}
+        for key in dataDict.keys():
+            airlineList = []
+            for opinion in dataDict[key]:
+                for word in opinion.split(" "):
+                    if (word.strip() not in stopWords):
+                        airlineList.append(word)
+            cleandDataDictForCorpusWithoutStopWords[key] = ' '.join(airlineList)
