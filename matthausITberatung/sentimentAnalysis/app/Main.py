1eb2c65ede625b13d74301c7b0dabc125500bf23
macpietr <mac.pietruszewski@gmail.com>
2022-01-07 22:07:23 +0100

    add TODO

--- matthausITberatung/sentimentAnalysis/app/Main.py
+++ matthausITberatung/sentimentAnalysis/app/Main.py
@@ -9,3 +9,5 @@ for key in airlinesDictOfListsOfMainOpinions.keys():
     airlinesDictOfZippedLists[key] = list(zip(airlinesDictOfListsOfDateFlown[key],airlinesDictOfListsOfMainOpinions[key]))
 
 print(airlinesDictOfZippedLists['lufthansa'][998])
+
+#TODO: use DateFlown and MainOpinions to sentiment analysis plots in time. Use DateFlown as time and opinions as positive or negative indicator of sentiment analysis
