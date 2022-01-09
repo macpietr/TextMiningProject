bff5e29705139e989fc530523e3f10ffab153de5
macpietr <mac.pietruszewski@gmail.com>
2022-01-09 12:19:08 +0100

    Change approach in case of calculating monthly polarity

--- matthausITberatung/sentimentAnalysis/app/Main.py
+++ matthausITberatung/sentimentAnalysis/app/Main.py
@@ -14,20 +14,20 @@ del lufthansaListOfMainOpinions[len(lufthansaListOfMainOpinions)-1]
 lufthansaListOfDateFlown = airlinesDictOfListsOfDateFlown['lufthansa']
 del lufthansaListOfDateFlown[len(lufthansaListOfDateFlown) - 1]
 
-lufthansaListOfMainOpinionsPolarity = []
-for line in lufthansaListOfMainOpinions:
-    lufthansaListOfMainOpinionsPolarity.append(TextBlob(line).sentiment.polarity)
+# lufthansaListOfMainOpinionsPolarity = []
+# for line in lufthansaListOfMainOpinions:
+#     lufthansaListOfMainOpinionsPolarity.append(TextBlob(line).sentiment.polarity)
 
-lufthansaZippedList = list(zip(lufthansaListOfDateFlown,lufthansaListOfMainOpinionsPolarity))
+lufthansaZippedList = list(zip(lufthansaListOfDateFlown,lufthansaListOfMainOpinions))
 
-lufthansaZippedList2021 = []
-for line in lufthansaZippedList:
-    if line[0][len(line)-6:] == '2021':
-        lufthansaZippedList2021.append(line)
+lufthansaZippedListOfDateFlownAndOpinion2021 = []
+for dateFlownAndOpinion in lufthansaZippedList:
+    if dateFlownAndOpinion[0][len(dateFlownAndOpinion) - 6:] == '2021':
+        lufthansaZippedListOfDateFlownAndOpinion2021.append(dateFlownAndOpinion)
 
-print(lufthansaZippedList2021)
+print(lufthansaZippedListOfDateFlownAndOpinion2021)
 
-lufthansaListOfDateFlown2021, lufthansaListOfMainOpinionsPolarity2021 = zip(*lufthansaZippedList2021)
+lufthansaListOfDateFlown2021, lufthansaListOfMainOpinions2021 = zip(*lufthansaZippedListOfDateFlownAndOpinion2021)
 
 print(lufthansaListOfDateFlown2021)
 
@@ -36,19 +36,42 @@ lufthansaDateFlown2021Counter = Counter(lufthansaListOfDateFlown2021)
 print(lufthansaDateFlown2021Counter)
 print(lufthansaDateFlown2021Counter.keys())
 
-dictOfDateFlownAndAvgPolarity = {}
-for key in lufthansaDateFlown2021Counter.keys():
-    listToSum = []
-    for line in lufthansaZippedList2021:
-        if str(line[0]) == str(key):
-            listToSum.append(line[1])
-    dictOfDateFlownAndAvgPolarity[key] = sum(listToSum)/lufthansaDateFlown2021Counter[key]
+dataDictOfDateFlownAndCollectedOpinions2021 = {}
+for dateFlown in lufthansaDateFlown2021Counter.keys():
+    listOfOpinionsForCertainDate = []
+    for dateFlownAndOpinion in lufthansaZippedListOfDateFlownAndOpinion2021:
+        if str(dateFlownAndOpinion[0]) == str(dateFlown):
+            listOfOpinionsForCertainDate.append(dateFlownAndOpinion[1])
+    dataDictOfDateFlownAndCollectedOpinions2021[dateFlown] = ' '.join(listOfOpinionsForCertainDate)
+#     print('')
+#
+# for item in dataDictOfDateFlownAndCollectedOpinions2021.items():
+#     print(item)
 
-print(dictOfDateFlownAndAvgPolarity)
+dataDictOfDateFlownAndCollectedOpinions2021Polarity = {}
+for dateFlown in dataDictOfDateFlownAndCollectedOpinions2021.keys():
+    dataDictOfDateFlownAndCollectedOpinions2021Polarity[dateFlown] = \
+        TextBlob(dataDictOfDateFlownAndCollectedOpinions2021[dateFlown]).sentiment.polarity
 
-plt.plot(dictOfDateFlownAndAvgPolarity.keys(),dictOfDateFlownAndAvgPolarity.values())
+[print(item) for item in dataDictOfDateFlownAndCollectedOpinions2021Polarity.items()]
+
+# dictOfDateFlownAndAvgPolarity = {}
+# for dateFlown in lufthansaDateFlown2021Counter.keys():
+#     listToSum = []
+#     for dateFlownAndOpinion in lufthansaZippedListOfDateFlownAndOpinion2021:
+#         if str(dateFlownAndOpinion[0]) == str(dateFlown):
+#             listToSum.append(dateFlownAndOpinion[1])
+#     dictOfDateFlownAndAvgPolarity[dateFlown] = sum(listToSum) / lufthansaDateFlown2021Counter[dateFlown]
+
+# print(dictOfDateFlownAndAvgPolarity)
+#
+# plt.plot(dictOfDateFlownAndAvgPolarity.keys(),dictOfDateFlownAndAvgPolarity.values())
+# plt.show()
+
+plt.plot(dataDictOfDateFlownAndCollectedOpinions2021Polarity.keys(),dataDictOfDateFlownAndCollectedOpinions2021Polarity.values())
 plt.show()
 
 # print(lufthansaZippedList[0][0][len(lufthansaZippedList[0][0])-4:])
 
 # TODO: check what will happen if we would count poalrity from post created form 17 posts insted of average polarity
+# TODO: the outcomes of polarity of concatenated ofinions differs from average polarity of separate opinions. Which measure is more relevant?
